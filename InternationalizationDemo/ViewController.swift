727e94ad68ba498933a5f44cadda2379cdfa1dd8
Erik <erikfloresq@gmail.com>
2016-01-29 15:42:49 -0500

    Initial Commit

---
+++ InternationalizationDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InternationalizationDemo
+//
+//  Created by Erik Flores on 1/29/16.
+//  Copyright © 2016 Orbis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
